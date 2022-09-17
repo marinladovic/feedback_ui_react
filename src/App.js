68b045d7ff66171fb88d7ae13cd65e5db75e5234
marin <mladovic.dev@gmail.com>
2022-09-17 18:45:29 +0200

    use application level state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <div className="container">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   );
 }
 
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate obcaecati quis recusandae, pariatur voluptatum eum!',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate obcaecati quis recusandae, pariatur voluptatum eum!',
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate obcaecati quis recusandae, pariatur voluptatum eum!',
+  },
+];
+
+export default FeedbackData;
